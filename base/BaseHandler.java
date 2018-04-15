02aef3c96a59b9e4e144c4807fcf08575ddc08ca
shuaisong <ysh5946@sina.com>
2018-04-15 18:38:55 +0800

    私照簡單獲取與現實

---
+++ base/BaseHandler.java
@@ -0,0 +1,19 @@
+package com.example.lenovo.myapplication.base;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * Created by lenovo on 2018/4/14.
+ */
+
+public class BaseHandler extends Handler {
+    @Override
+    public void handleMessage(Message msg) {
+        if (msg.arg1==200){
+
+        }
+
+    }
+}
